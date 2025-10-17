8f56952c50c58b9886667509071f2381c59f11a5
FrancoAquino23 <francoaquialla@gmail.com>
2025-10-17 08:11:32 -0600

    Initial Commit

---
+++ Music/MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicApp.swift
+//  Music
+//
+//  Created by Fran on 17/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
